ff18a8c61c5d04db97e0cb474b8f23bd055ecf55
Anandan <anand.thepetal@gmail.com>
2023-08-26 13:30:49 +0530

    26/08/2023-commited

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,23 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+let name="kayal";
+let email="kayalpetalautomations@gmail.com";
+let age=22;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div style={{display:"flex",fontSize:"16px",alignItems:'center',flexDirection:'column', margin:'20px'}}>
+   {name}
+      <input placeholder='Enter the Name'></input>
+      <input placeholder='Enter the MobileNo'></input>
+      <input placeholder='Enter the Gmail-Id'></input>
+      <input placeholder='Enter the Address'></input>
+      <button onClick={() => {console.log('OnClieck Is working')}}>Save</button>
+    
     </div>
+  
   );
 }
 
